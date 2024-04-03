1e5dab5b354f8ae2978fd203046ff6060e3d3b4f
Ashani M <ashanim@Ashanis-Air.home>
2024-04-03 12:38:36 -0400

    Accessed data from data.js and rendered it onto html page

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import puppyList from "./data";
+import './App.css'
+
+function App() {
+const [puppies,setPuppies] = useState(puppyList)
+console.log(puppies)
+
+  return (
+    <>
+      <div>
+        {puppies.map((puppy) => {
+          return <p>  key = {puppy.id} {puppy.name}</p>
+        })
+        
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
