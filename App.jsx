dc7ddce23a38728a458688cc76e2b5d4227f3419
Ashani M <ashanim@Ashanis-Air.home>
2024-04-03 16:37:58 -0400

    Accesed and Rendered Pups and Pups informatin on page

--- App.jsx
+++ App.jsx
@@ -4,19 +4,35 @@ import './App.css'
 
 function App() {
 const [puppies,setPuppies] = useState(puppyList)
+const [featPupId, setFeatPupId] = useState(null)
 console.log(puppies)
 
-  return (
-    <>
-      <div>
-        {puppies.map((puppy) => {
-          return <p>  key = {puppy.id} {puppy.name}</p>
-        })
-        
-        }
-      </div>
-    </>
-  )
+const handleClick = (puppy) => {
+  console.log("puppy id:" , puppy.id)
+  setFeatPupId(puppy.id)
 }
+
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+console.log(featuredPup)
+return (
+  <>
+    <div id='puplist'>
+      {puppies.map((puppy) => (
+        <p onClick={() => handleClick(puppy)} key={puppy.id}>{puppy.name}</p>
+      ))}
+    </div>
+    {featPupId && featuredPup && (
+      <div id='pupinfo'>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>{featuredPup.age}</li>
+          <li>{featuredPup.email}</li>
+        </ul>
+      </div>
+    )}
+  </>
+);
+    }
+
 
 export default App
